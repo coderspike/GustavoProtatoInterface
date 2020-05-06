7e22f44df8fc48e8224abe403e658850af3f52ae
wenbin guo <wenbinguo@wenbindeMacBook-Pro.local>
2020-05-06 20:28:48 +0800

    code init

---
+++ src/main/java/com/gustavo/protato/ProtatoApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.protato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtatoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtatoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gustavo/protato/ProtatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.protato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtatoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
