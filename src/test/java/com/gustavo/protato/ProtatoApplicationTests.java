9ce67af3083cabcbc320f523065e4b5179854fac
wenbin guo <wenbinguo@wenbindeMacBook-Pro.local>
2020-05-12 20:36:55 +0800

    cin

--- src/test/java/com/gustavo/protato/ProtatoApplicationTests.java
+++ src/test/java/com/gustavo/protato/ProtatoApplicationTests.java
@@ -3,11 +3,15 @@ package com.gustavo.protato;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Objects;
+
 @SpringBootTest
 class ProtatoApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(Objects.equals(null, "小六六"));
+
     }
 
 }
